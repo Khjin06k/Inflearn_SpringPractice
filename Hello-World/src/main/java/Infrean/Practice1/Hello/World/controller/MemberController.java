b444731b6c5ac49897d4f4c5461d0ec32c556686
김희진 <khj4209kk@gmail.com>
2022-11-01 00:09:42 +0900

    섹센5 - 회원 웹 기능(조회)

--- Hello-World/src/main/java/Infrean/Practice1/Hello/World/controller/MemberController.java
+++ Hello-World/src/main/java/Infrean/Practice1/Hello/World/controller/MemberController.java
@@ -4,7 +4,11 @@ import Infrean.Practice1.Hello.World.Service.MemberService;
 import Infrean.Practice1.Hello.World.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링 컨테이너 라는 통해 MemberController 객체를 생성해 스프링에 넣어 관리하도록 하는 애너테이션
 public class MemberController { //Controller는 Service를 통해서 컨트롤러가 기능을 가져와 실행이 가능하기에 Service를 가져와 사용해야 한다.
@@ -23,6 +27,7 @@ public class MemberController { //Controller는 Service를 통해서 컨트롤�
         return "members/createMemberForm";
     }
 
+    @PostMapping("/members/new")
     public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName()); //form에서 가져와 멤버 이름에 지정
@@ -30,5 +35,13 @@ public class MemberController { //Controller는 Service를 통해서 컨트롤�
         memberService.join(member); //지정된 이름을 가지고 Service에 있는 join을 통해 member을 등록함
 
         return "/redirect:/"; //등록 이후 다시 홈 화면으로 이동
+    }
+
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); //model을 모두 List에 담아서 넘김
+
+        return "members/memberList";
     }
 }
